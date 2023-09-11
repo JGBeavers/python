71085eeb51e5864bca2c718d62ef6ea56a0c31cb
JGBeavers <67471068+JGBeavers@users.noreply.github.com>
2023-09-10 23:33:49 -0400

    Add files via upload

---
+++ data-structure-exercise/07_multiple_letter_count/multiple_letter_count.py
@@ -0,0 +1,20 @@
+def multiple_letter_count(phrase):
+    """Return dict of {ltr: frequency} from phrase.
+
+        >>> multiple_letter_count('yay')
+        {'y': 2, 'a': 1}
+
+        >>> multiple_letter_count('Yay')
+        {'Y': 1, 'a': 1, 'y': 1}
+    """
+    letter_count = {}
+    for ltr in phrase:
+        letter_count[ltr] = letter_count.get(ltr, 0) + 1
+        
+    print(letter_count)
+    return letter_count
+    
+    
+
+multiple_letter_count('yay')
+multiple_letter_count('Yay')
